28153c60cb2055d6f6a69d2b3ac3a580cf79e6c3
rahulbarmann <itsrahulbarman1@gmail.com>
2024-10-29 00:58:22 +0530

    refactor: setup tailwind CSS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,17 @@
 import '@/styles/globals.css'
 
-import { tokenFaucetConfig } from '@/services/utils';
-import { AlephiumWalletProvider } from '@alephium/web3-react';
-import React from 'react';
+// import { tokenFaucetConfig } from '@/services/utils'
+// import { AlephiumWalletProvider } from '@alephium/web3-react'
+import React from 'react'
 
 export const metadata = {
-  title: "Alephium dApp Template",
-  description: "A template for building dApps on Alephium",
-};
-
+  title: 'Alephium dApp Template',
+  description: 'A template for building dApps on Alephium'
+}
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
@@ -20,10 +19,14 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body>
-        <AlephiumWalletProvider theme="web95" network={tokenFaucetConfig.network} addressGroup={tokenFaucetConfig.groupIndex}>
-          {children}
-        </AlephiumWalletProvider>
+        {/* <AlephiumWalletProvider
+          theme="web95"
+          network={tokenFaucetConfig.network}
+          addressGroup={tokenFaucetConfig.groupIndex}
+        > */}
+        {children}
+        {/* </AlephiumWalletProvider> */}
       </body>
     </html>
-  );
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import Home from '@/components/Home';
+import React from 'react'
 
 export default function HomePage() {
   return (
     <>
-      <Home />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   )
 }
