eba9f1f4374026a5534201520ffd04463c953cdd
rahulbarmann <itsrahulbarman1@gmail.com>
2024-10-29 01:23:12 +0530

    feat: setup alephium wallet adapter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.css'
 
 // import { tokenFaucetConfig } from '@/services/utils'
-// import { AlephiumWalletProvider } from '@alephium/web3-react'
+import { AlephiumWalletProvider } from '@alephium/web3-react'
 import React from 'react'
 
 export const metadata = {
@@ -19,13 +19,13 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body>
-        {/* <AlephiumWalletProvider
-          theme="web95"
-          network={tokenFaucetConfig.network}
-          addressGroup={tokenFaucetConfig.groupIndex}
-        > */}
-        {children}
-        {/* </AlephiumWalletProvider> */}
+        <AlephiumWalletProvider
+          theme="retro"
+          network={'testnet'}
+          // addressGroup={tokenFaucetConfig.groupIndex}
+        >
+          {children}
+        </AlephiumWalletProvider>
       </body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { AlephiumConnectButton } from '@alephium/web3-react'
 import React from 'react'
 
 export default function HomePage() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <AlephiumConnectButton />
     </>
   )
 }
